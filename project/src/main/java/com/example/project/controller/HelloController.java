7102448d10d5757738dfcc625c120e34461aebc2
yoonminsoo97 <yoonminwater@gmail.com>
2024-10-31 21:10:37 +0900

    feat: 간단한 컨트롤러 추가

---
+++ project/src/main/java/com/example/project/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.project.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> sayHelloWorld() {
+        return ResponseEntity.ok().body("Hello World!!");
+    }
+
+}
