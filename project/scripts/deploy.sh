c78e7341a6f2122f8c003a2ad4d30423abbea680
yoonminsoo97 <yoonminwater@gmail.com>
2024-11-05 23:03:08 +0900

    chore: update deploy

--- project/scripts/deploy.sh
+++ project/scripts/deploy.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
 
+# REPOSITORY 변수에 JAR 파일이 저장된 디렉토리 경로 지정
 REPOSITORY=/home/ubuntu/app/build/libs
+
+# PROJECT_NAME 변수에 프로젝트 이름 지정
 PROJECT_NAME="project"
 
-chmod +x $REPOSITORY/project-0.0.1-SNAPSHOT.jar
+# 현재 실행 중인 애플리케이션의 PID 확인
+CURRENT_PID=$(pgrep -f "java -jar $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar")
 
-# chomd +x /home/ubuntu/app/build/libs/proj
+# 현재 애플리케이션이 실행 중이면 종료
+if [ -n "$CURRENT_PID" ]; then
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
 
-echo "> JAR 실행"
-nohup java -jar $REPOSITORY/project-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
+# JAR 파일 이름 가져오기
+JAR_NAME=$(ls -tr $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar | tail -n 1)
 
-# echo "> 현재 구동 중인 애플리케이션 pid 확인"
+# JAR 파일에 실행 권한 추가
+chmod +x $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar
 
-# CURRENT_PID=$(pgrep -f "java -jar $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar")
-
-# echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-# if [ -z "$CURRENT_PID" ]; then
-#   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-# else
-#   echo "> kill -15 $CURRENT_PID"
-#   kill -15 $CURRENT_PID
-#   sleep 5
-# fi
-
-# echo "> 새 애플리케이션 배포"
-
-# JAR_NAME=$(ls -tr $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar | tail -n 1)
-
-# echo "> JAR NAME: $JAR_NAME"
-
-# echo "> $JAR_NAME 에 실행권한 추가"
-
-# chmod +x "$REPOSITORY/$JAR_NAME"
-
-# echo "> $JAR_NAME 실행"
-
-# nohup java -jar "$REPOSITORY/$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
+# JAR 파일 실행
+nohup java -jar $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar > $REPOSITORY/nohup.out 2>&1 &
