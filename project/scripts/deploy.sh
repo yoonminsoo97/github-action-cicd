c597ac47b2312749b0876e12dbccf7ac7da026c6
yoonminsoo97 <yoonminwater@gmail.com>
2024-11-05 22:07:43 +0900

    chore: deploy.sh

--- project/scripts/deploy.sh
+++ project/scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "> Hello, world!!"
+# echo "> Hello, world!!"
 
 # touch hello.md
 
-# REPOSITORY=/home/ubuntu/app/build/libs
-# PROJECT_NAME= "project"
+REPOSITORY=/home/ubuntu/app/build/libs
+PROJECT_NAME= "project"
 
 # echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
@@ -23,14 +23,14 @@ echo "> Hello, world!!"
 
 # echo "> 새 애플리케이션 배포"
 
-# JAR_NAME=$(ls -tr $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/$PROJECT_NAME-0.0.1-SNAPSHOT.jar | tail -n 1)
 
 # echo "> JAR NAME: $JAR_NAME"
 
 # echo "> $JAR_NAME 에 실행권한 추가"
 
-# chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
 # echo "> $JAR_NAME 실행"
 
-# nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
